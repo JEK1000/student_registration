f6476540606d19112e2340d47d6f076337483864
Jason <jasonekildare@gmail.com>
2023-06-07 14:13:44 -0400

    Update log_api.js

--- src/components/login/log_api.js
+++ src/components/login/log_api.js
@@ -4,7 +4,7 @@ export const login = async (FormData) => {
   try {
     const response = await axios.post(
       'https://nodeserv-production.up.railway.app/stud',
-      FormData,
+      FormData
       {
         headers: {
           "Content-Type": "application/json"
