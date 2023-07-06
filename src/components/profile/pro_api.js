9b0b821fb27d9a7483982d8a0d977ee7229f4b30
Jason <jasonekildare@gmail.com>
2023-07-06 18:47:54 -0400

    Update pro_api.js

--- src/components/profile/pro_api.js
+++ src/components/profile/pro_api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const profile = async (FormData, id) => {
   try {
-    const response = await axios.put(`https://nodeserver8.onrender.com/api/update_user/${id}`, { 
+    const response = await axios.put(`https://nodeserv-production.up.railway.app/api/update_user/${id}`, { 
       FormData
     });
     return response.data;
